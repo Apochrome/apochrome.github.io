945f4817576438f31cd657e74805e3fe01c917d1
Apochrome <swann.nicholas@gmail.com>
2025-05-20 21:32:28 -0600

    Updates

--- images/convert_all.sh
+++ images/convert_all.sh
@@ -1,5 +1,5 @@
 for img in *.jpg; do
     filename="${img%.*}"
-    magick "$img" -resize 1200x -quality 75 "${filename}.webp"
+    magick "$img" -resize 2200x -quality 100 "${filename}.webp"
     echo "Converted: $img -> ${filename}.webp"
 done
